c3759a77d5a0408a159bd84a782b04edf553a877
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2024-07-10 17:37:16 +0530

    add elytra layer mixin

---
+++ src/main/java/com/mystchonky/machina/mixin/perk/ElytraLayerMixin.java
@@ -0,0 +1,19 @@
+package com.mystchonky.machina.mixin.perk;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mystchonky.machina.common.armament.perk.PerkLibrary;
+import net.minecraft.client.renderer.entity.layers.ElytraLayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ElytraLayer.class)
+public abstract class ElytraLayerMixin {
+
+    @ModifyReturnValue(method = "shouldRender", at = @At("RETURN"))
+    private boolean renderElytra(boolean original, ItemStack stack, LivingEntity entity) {
+        return original || PerkLibrary.hasPerk(entity, PerkLibrary.GLIDE);
+    }
+
+}
