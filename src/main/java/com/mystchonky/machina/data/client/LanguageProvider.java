14b6048d212b61e2961830a0f10e7709f85c84a5
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2024-12-10 19:52:34 -0700

    feat: add rift lang entries

--- src/main/java/com/mystchonky/machina/data/client/LanguageProvider.java
+++ src/main/java/com/mystchonky/machina/data/client/LanguageProvider.java
@@ -2,6 +2,7 @@ package com.mystchonky.machina.data.client;
 
 import com.mystchonky.machina.Machina;
 import com.mystchonky.machina.common.armament.perk.PerkLibrary;
+import com.mystchonky.machina.common.registrar.BlockRegistrar;
 import com.mystchonky.machina.common.registrar.GearRegistrar;
 import com.mystchonky.machina.common.registrar.ItemRegistrar;
 import com.mystchonky.machina.common.registrar.LangRegistrar;
@@ -18,6 +19,9 @@ public class LanguageProvider extends net.neoforged.neoforge.common.data.Languag
         add(ItemRegistrar.VOID_CHESTPLATE.get(), "Voidwalker Mantle");
         add(ItemRegistrar.VOID_LEGGINGS.get(), "Voidwalker Breeches");
         add(ItemRegistrar.VOID_BOOTS.get(), "Voidwalker Greaves");
+        add(ItemRegistrar.RIFT_PEARL.get(), "Rift Pearl");
+
+        add(BlockRegistrar.RIFT.block(), "Rift");
 
         GearRegistrar.GEARS.getEntries().forEach(gear -> {
             add(gear.get().getGearItem(), gear.get().displayName());
